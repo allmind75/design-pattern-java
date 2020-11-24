dc881336835817b71b6e3897d3b918ec75c25e7b
장환호 <allmind75@interpark.com>
2020-11-24 14:56:53 +0900

    ConcurrentModificationException 이슈 수정

--- src/observer/case2/step2/PlayController.java
+++ src/observer/case2/step2/PlayController.java
@@ -1,47 +1,44 @@
 package observer.case2.step2;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class PlayController implements Publisher {
-	private List<Observer> observers = new ArrayList<>();
-	private boolean play;
-	
-	@Override
-	public void addObserver(Observer observer) {
-		observers.add(observer);
-	}
+    /*
+        CopyOnWriteArrayList : https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/CopyOnWriteArrayList.html
+        - Array add, remove 시 객체를 복사해서 사용
+        - add, remove가 많이 발생하는 경우 성능저하 발생
 
-	@Override
-	public void deleteObserver(Observer observer) {
-		int index = observers.indexOf(observer);
-		observers.remove(index);
-	}
+        아래의 코드에서 ArrayList로 설정후 notifyObservers 호출시 ConcurrentModificationException 발생
+        - ConcurrentModificationException : https://docs.oracle.com/javase/7/docs/api/java/util/ConcurrentModificationException.html
+        - notifyObservers > forEach 내부에서 Iterator를 이용하는데 deleteObserver가 호출되면서 사용중인 collection이 수정되면서 오류 발생
+    */
+    private List<Observer> observers = new CopyOnWriteArrayList<>();
+    private boolean play;
 
-	@Override
-	public void notifyObservers() {
-		for (int i=0; i<observers.size(); i++) {
-			observers.get(i).update(play);
-		}
-	}
+    @Override
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
 
-	public void setFlag(boolean play) {
-		this.play = play;
-		notifyObservers();
-	}
-	
-	public boolean getFlag() {
-		return play;
-	}
+    @Override
+    public void deleteObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(play);
+        }
+    }
+
+    public void setFlag(boolean play) {
+        this.play = play;
+        notifyObservers();
+    }
+
+    public boolean getFlag() {
+        return play;
+    }
 }
-
-/*
-  	for (int i=0; i<observers.size(); i++) {
-		observers.get(i).update(play);
-	}
-	// 다음을 사용하면 구독 해지시 다음과 같은 에러가 발생한다.
-	// ConcurrentModificationException
-	for (Observer observer : observers) {
-		observer.update(play);
-	}
-*/
